c5a22b96a3c9756a1b1567fd00da2c48ce2a41c4
petro <petro.kryvolap95@gmail.com>
2019-01-20 20:31:14 +0200

    - change 1

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('*', (req, res) => {
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404))
 });
 
 // error handler
